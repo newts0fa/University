671683930a58ef7fd8ece183e50306a06b21a24e
newts0fa <nbtvor@yandex.ru>
2024-12-09 23:47:26 +0300

    Update main.py

--- Practice/practice6/main.py
+++ Practice/practice6/main.py
@@ -1,47 +1,29 @@
-import os
-import sys
+from pomosh import *
 
-# Убедимся, что необходимые библиотеки установлены
-try:
-    import PyPDF2
-    from docx import Document
-except ImportError:
-    # Установка необходимых библиотек
-    os.system(f"{sys.executable} -m pip install PyPDF2 python-docx")
-    import PyPDF2
-    from docx import Document
-
-def pdf_to_doc(pdf_file, doc_file):
-    """
-    Преобразование PDF в DOC.
-    :param pdf_file: Путь к исходному PDF-файлу.
-    :param doc_file: Путь для сохранения результата в DOC.
-    """
-    try:
-        # Чтение PDF-файла
-        reader = PyPDF2.PdfReader(pdf_file)
-        document = Document()
-
-        # Извлечение текста со страниц PDF
-        for page in reader.pages:
-            text = page.extract_text()
-            if text.strip():  # Проверяем, есть ли текст на странице
-                document.add_paragraph(text)
-
-        # Сохранение результата в DOC-файле
-        document.save(doc_file)
-        print(f"PDF успешно преобразован в DOC: {doc_file}")
-
-    except Exception as e:
-        print(f"Ошибка при преобразовании PDF в DOC: {e}")
-
-# Пример использования
-if name == "__main__":
-    pdf_file_path = "sample.pdf"  # Путь к PDF-файлу
-    doc_file_path = "output.docx"  # Путь к выходному DOC-файлу
-    
-    # Проверяем существование PDF-файла
-    if not os.path.exists(pdf_file_path):
-        print(f"Файл {pdf_file_path} не найден. Проверьте путь.")
-    else:
-        pdf_to_doc(pdf_file_path, doc_file_path)
+while True:
+    plesure = files.return_curr_dir()
+    print(f'Текущий каталог: {plesure}')
+    print('Выберите действие:\n\n0. Сменить рабочий каталог\n1. Преобразовать PDF в Dock\n2. Преобразовать Docx в '
+          'PDF\n3. Произвести сжатие изображений\n4. Удалить группу файлов\n5. Выход')
+    while True:
+        choice = input('Выбор: ')
+        if not choice.isdigit() or choice not in ['0', '1', '2', '3', '4', '5']:
+            print('Введено неправильное действие!')
+        else:
+            break
+    if choice == '0':
+        files.change_dir()
+    elif choice == '1':
+        PDF_docx.pdf_to_docx()
+    elif choice == '2':
+        PDF_docx.docx_to_pdf()
+    elif choice == '3':
+        img_comprs.compress_img()
+    elif choice == '4':
+        files.delete_files()
+    elif choice == '5':
+        print('Goodbye :)')
+        exit()
+    else:  # на случай 'невозможного' случая, easter egg так сказать, ignore...
+        print("\033[35m{}".format(' ' * 10 + 'Challenge Complete!\n'),
+              "\033[0m{}".format(' ' * 9 + 'How Did We Get Here?'))
